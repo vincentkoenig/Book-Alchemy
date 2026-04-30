1b12b17fe1896dd65b378442e4f3123d1387e48e
Vincent König <vincentkoenig97@gmail.com>
2026-04-30 18:59:08 +0200

    Initialize Flask app and configure SQLAlchemy database

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from data_models import db, Author, Book
+import os
+app = Flask(__name__)
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{os.path.join(basedir, 'data/library.sqlite')}"
+db.init_app(app)
+
+
+#with app.app_context():
+  #db.create_all()

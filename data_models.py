18d43be9082296b68ab84a07e6f3bfdce6d3ede7
Vincent König <vincentkoenig97@gmail.com>
2026-05-18 18:51:43 +0200

    Fix isbn to String, add nullable=False and unique=True constraints

--- data_models.py
+++ data_models.py
@@ -6,15 +6,15 @@ class Author(db.Model):
     __tablename__ = 'authors'
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    name = db.Column(db.String)
+    name = db.Column(db.String, nullable=False)
     birth_date = db.Column(db.Date)
     date_of_death = db.Column(db.Date)
 
 class Book(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    author_id = db.Column(db.Integer, db.ForeignKey('authors.id'))
-    isbn = db.Column(db.Integer)
-    title = db.Column(db.String)
+    author_id = db.Column(db.Integer, db.ForeignKey('authors.id'), nullable=False)
+    isbn = db.Column(db.String, nullable=False, unique=True)
+    title = db.Column(db.String, nullable=False)
     publication_year = db.Column(db.Integer)
     author = db.relationship('Author', backref='books')
 
